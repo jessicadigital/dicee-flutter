4720ad017330c94b4a3dc727bd523368f55ec3da
Dr Jessica Muirhead <github@jessica.digital>
2024-05-24 17:01:50 +0100

    Flutter Bootcamp: Lesson 62 Challenge 3

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,13 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 2;
 
+  void rollDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;
+      rightDiceNumber = Random().nextInt(6)+1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -37,10 +44,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6)+1;
-                  rightDiceNumber = Random().nextInt(6)+1;
-                });
+                rollDice();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -48,10 +52,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6)+1;
-                  rightDiceNumber = Random().nextInt(6)+1;
-                });
+                rollDice();
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
