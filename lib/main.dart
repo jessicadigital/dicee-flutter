cc3b978f0f22dde737c9af2fc871c12304b1a3ff
Dr Jessica Muirhead <github@jessica.digital>
2024-05-24 15:44:55 +0100

    Flutter Bootcamp: Lesson 53 Gesture Detection

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
           foregroundColor: Colors.white,
-
         ),
         body: DicePage(),
       ),
@@ -25,14 +24,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child:Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print('Left');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child:Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print('Right');
+              },
               child: Image.asset('images/dice2.png'),
             ),
           ),
